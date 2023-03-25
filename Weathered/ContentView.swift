7b45db586870393f354d579826c9056b6b65f510
bodhichristian <110639779+bodhichristian@users.noreply.github.com>
2023-03-25 10:35:21 -0400

    Weathered
    Initial commit

---
+++ Weathered/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Weathered
+//
+//  Created by christian on 3/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weathered/WeatheredApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatheredApp.swift
+//  Weathered
+//
+//  Created by christian on 3/25/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatheredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
