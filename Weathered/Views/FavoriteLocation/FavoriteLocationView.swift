04492e5018c624a6381b670e0a4de848766380ec
bodhichristian <clavelle23@gmail.com>
2023-08-22 19:50:48 -0400

    FavoriteLocationView now has access to local time of day, and uses it to conditionally render daytime or nighttime weather icons.

--- Weathered/Views/FavoriteLocation/FavoriteLocationView.swift
+++ Weathered/Views/FavoriteLocation/FavoriteLocationView.swift
@@ -14,50 +14,57 @@ struct FavoriteLocationView: View {
     
     @State private var weatherData: WeatherData?
     
+    var daytime: Bool {
+        (weatherData?.location.localtime.calculateTimeOfDay() ?? 0.5) > 0.25
+        && (weatherData?.location.localtime.calculateTimeOfDay() ?? 0.5 < 0.75)
+    }
+    
     var body: some View {
-            ZStack(alignment: .bottomLeading) {
-                RoundedRectangle(cornerRadius: 10)
-                    .frame(width: 150, height: 100)
-                    .foregroundStyle(.thinMaterial)
-                    .shadow(color: .primary.opacity(0.5), radius: 2)
-                
-                
-                VStack(alignment: .leading) {
-                    HStack {
-                        if let conditionCode = weatherData?.current.condition.code {
-                            Image(systemName: WeatherIconsDaytime[conditionCode] ?? "sun")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 30, height: 30)
-                        }
-                        
-                        if let temp = weatherData?.current.tempF {
-                            Text("\(Int(temp))°F")
-                        }
+        ZStack(alignment: .bottomLeading) {
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: 150, height: 100)
+                .foregroundStyle(.thinMaterial)
+                .shadow(color: .primary.opacity(0.5), radius: 2)
+            
+            
+            VStack(alignment: .leading) {
+                HStack {
+                    if let conditionCode = weatherData?.current.condition.code {
+                        Image(systemName:  daytime // Between 6:00am and 6:00pm
+                              ? WeatherIconsDaytime[conditionCode] ?? "sun.fill"
+                              : WeatherIconsNighttime[conditionCode] ?? "moon.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 30, height: 30)
                     }
-                    .font(.title2)
-                    .bold()
-
-                    Text(location.name)
-                        
-                        .font(.headline)
-                }
-                .fontDesign(fontDesign)
-                .padding()
-            }
-            .padding(.vertical)
-            .onAppear {
-                withAnimation(.bouncy) {
-                    weatherService.fetchWeatherData(for: location.name) { result in
-                        switch result {
-                        case .success(let data):
-                            weatherData = data
-                        case .failure(let error):
-                            print(error.localizedDescription)
-                        }
+                    
+                    if let temp = weatherData?.current.tempF {
+                        Text("\(Int(temp))°F")
                     }
                 }
+                .font(.title2)
+                .bold()
+                
+                Text(location.name)
+                
+                    .font(.headline)
             }
+            .fontDesign(fontDesign)
+            .padding()
+        }
+        .padding(.vertical)
+        .onAppear {
+            withAnimation(.bouncy) {
+                weatherService.fetchWeatherData(for: location.name) { result in
+                    switch result {
+                    case .success(let data):
+                        weatherData = data
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+        }
     }
 }
 
