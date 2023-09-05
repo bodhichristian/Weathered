7057d1f09e7476ebfb213511e9c5bb8164a22930
bodhichristian <clavelle23@gmail.com>
2023-09-05 08:32:11 -0400

    Create CurrentLocationView to display in HomeView when user has location services enabled.

---
+++ Weathered/Views/Locations/CurrentLocationView.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentLocationView.swift
+//  Weathered
+//
+//  Created by christian on 9/5/23.
+//
+
+import SwiftUI
+
+struct CurrentLocationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CurrentLocationView()
+}
